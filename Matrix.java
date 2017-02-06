a67f75fde732711e1bb498ff684abab206429e2e
Clint Hall <hallbc@appstate.edu>
2017-02-06 13:38:04 -0500

    Write Matrix stub

---
+++ Matrix.java
@@ -0,0 +1,151 @@
+class Matrix{
+
+    Matrix(double[][] A){
+    }
+    Matrix(double[][] A, int m, int n){
+    }
+    Matrix(double[] vals, int m){
+    }
+    Matrix(int m, int n){
+    }
+    Matrix(int m, int n, double s){
+    }
+
+    Matrix  arrayLeftDivide(Matrix B){
+        return null;
+    }
+    Matrix  arrayLeftDivideEquals(Matrix B){
+        return null;
+    }
+    Matrix  arrayRightDivide(Matrix B){
+        return null;
+    }
+    Matrix  arrayRightDivideEquaqals(Matrix B){
+        return null;
+    }
+    Matrix  arrayTimes(Matrix B){
+        return null;
+    }
+    Matrix  arrayTimesEquals(Matrix B){
+        return null;
+    }
+    java.lang.Object    clone(){
+        return null;
+    }
+    static Matrix   constructWithCopy(double[][] A){
+        return null;
+    }
+    Matrix  copy(){
+        return null;
+    }
+    double  get(int i, int j){
+        return null;
+    }
+    double[][]  getArray(){
+        return null;
+    }
+    double[][]  getArrayCopy(){
+        return null;
+    }
+    int     getColumnDimension(){
+        return null;
+    }
+    double[]    getColumnPackedCopy(){
+        return null;
+    }
+    Matrix  getMatrix(int[] r, int[] c){
+        return null;
+    }
+    Matrix  getMatrix(int[] r, int j0, int j1){
+        return null;
+    }
+    Matrix  getMatrix(int i0, int i1, int[] c){
+        return null;
+    }
+    Matrix  getMatrix(int i0, int i1, int j0, int j1){
+        return null;
+    }
+    int     getRowDimension(){
+        return null;
+    }
+    double[]    getRowPackedCopy(){
+        return null;
+    }
+    static Matrix   identity(int m, int n){
+        return null;
+    }
+    Matrix  minus(Matrix B){
+        return null;
+    }
+    Matrix  minusEquals(Matrix B){
+        return null;
+    }
+    double  norm1(){
+        return null;
+    }
+    double  normF(){
+        return null;
+    }
+    double  normInf(){
+        return null;
+    }
+    Matrix  plus(Matrix B){
+        return null;
+    }
+    Matrix  plusEquals(Matrix B){
+        return null;
+    }
+    void    print(int w, int d){
+        return null;
+    }
+    void    print(java.text.NumberFormat format, int width){
+        return null;
+    }
+    void    print(java.io.PrintWriter output, int w, int d){
+        return null;
+    }
+    void    print(java.io.PrintWriter output, java.text.NumberFormat format, int width){
+        return null;
+    }
+    static Matrix   random(int m, int n){
+        return null;
+    }
+    static Matrix   read(java.io.BufferedReader input){
+        return null;
+    }
+    void    set(int i, int j, double s){
+        return null;
+    }
+    void    setMatrix(int[] r, int[] c, Matrix X){
+        return null;
+    }
+    void    setMatrix(int[] r, int j0, int j1, Matrix X){
+        return null;
+    }
+    void    setMatrix(int i0, int i1, int[] c, Matrix X){
+        return null;
+    }
+    void    setMatrix(int i0, int i1, int j0, int j1, Matrix X){
+        return null;
+    }
+    Matrix  times(double s){
+        return null;
+    }
+    Matrix  times(Matrix B){
+        return null;
+    }
+    Matrix  timesEquals(double s){
+        return null;
+    }
+    double  trace(){
+        return null;
+    }
+    Matrix  transpose(){
+        return null;
+    }
+    Matrix  uminus(){
+        return null;
+    }
+}
+
+
