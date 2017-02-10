daf8e3238a39275e50c4505f26e1a6d46c0247f8
B. Clint Hall <bclinthall@gmail.com>
2017-02-10 15:27:25 -0500

    begin test class

---
+++ junit/ArithmaticTests.java
@@ -0,0 +1,32 @@
+package junit;
+import javatrix.Matrix;
+import static org.junit.Assert.assertEquals;
+import javatrix.Matrix;
+
+public class ArithmaticTests{
+    /**
+    * Test byElement(Matrix, Matrix, Operator).
+    */
+    @Test
+    void byElementTest1(){
+        double[][] a1 = {
+            {  1,   2,   3,   4}, 
+            {  5,   6,   7,   8}, 
+            {  9,  10,  11,  12}
+        };
+        Matrix m1 = new Matrix(a1);
+        double[][] a2 = {
+            { -3,  -4,  -5,  -6}, 
+            { -7,  -8,  -9, -10}, 
+            {-11, -12, -13, -14}
+        }
+        Matrix m2 = new Matrix(a2);
+        double[][] answer = {
+            { -2, -2, -2, -2},
+            { -2, -2, -2, -2},
+            { -2, -2, -2, -2}
+        }
+        Matrix m3 = Matrix.byElement(m1, m2, (a, b) -> a + b);
+        assertEquals(answer, m3.getArray());
+    }
+}
